779a60e3316583c055aae152876c6365e8439792
eromosene <danieleromosene@gmail.com>
2023-03-17 19:57:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints the last digit of a randomly generated number
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+
+}
