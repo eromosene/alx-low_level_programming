529e6dbf552ea5b96b0e0863af3d3971bc4b0084
eromosene <danieleromosene@gmail.com>
2023-03-22 18:47:10 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,18 @@ if (n <= 98)
 for (i = n; i <= 98; i++)
 {
 if (i != 98)
-printf(%d, , i);
+printf("%d, ", i);
 else if (i == 98)
-printf(%dn, i);
+printf("%dn", i);
 }
 } else if (n >= 98)
 {
 for (j = n; j >= 98; j--)
 {
 if (j != 98)
-printf(%d, , j);
+printf("%d, ", j);
 else if (j == 98)
-printf(%dn, j);
+printf("%dn", j);
 }
 }
 }
