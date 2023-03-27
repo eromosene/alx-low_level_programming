687c469f1685fb8974307008eeec5080cbf4f0be
eromosene <danieleromosene@gmail.com>
2023-03-27 16:14:13 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - takes in two variables and swaps them
+ * @author Daniel
+ * @a: swaps and stores address of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int swap;
+change = *b;
+*b = *a;
+*a = change;
+}
