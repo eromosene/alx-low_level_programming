4ffe952c700a6647facfdc3d4fdc679e5f0bbdd6
eromosene <danieleromosene@gmail.com>
2023-04-05 19:31:42 -0700

    101-wildcmp.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,24 +11,24 @@ int is_palindrome(char *s)
 {
 if (*s == 0)
 {
-return (1);
+return (1);
 }
-return (check_pal(s, 0, _strlen_recursion(s)));
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
 * _strlen_recursion - returns the length of a string
 * @s: string to calculate the length of
-* 
+*
 * Return: length of the string
 */
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s == '\0')
+if (*s == '\0')
 {
 return (0);
 }
-return (1 + _strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s + 1));
 }
 /**
 * check_pal - checks the characters recursively for palindrome
@@ -38,7 +38,7 @@ return (1 + _strlen_recursion(s + 1));
 *
 * Return: 1 if palindrome, 0 if not
 */
-int check_pal(char *s, int i, int len)
+int check_pal(char *s, int i, int len)
 {
 if (*(s + i) != *(s + len - 1))
 {
